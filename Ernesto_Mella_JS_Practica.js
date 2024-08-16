8d83dde5e012789ead9e7562c99d530bfd4d85a6
Ernexto-M-M <ernestogabrielmella@gmail.com>
2024-08-16 19:43:40 -0400

    Agregar Ernesto_Mella_JS_Practica.js

---
+++ Ernesto_Mella_JS_Practica.js
@@ -0,0 +1,10 @@
+let edadUsuario = 25;
+function puedeObtenerLicencia() {
+if (edadUsuario > 17) {
+console.log("¡Listo para obtener la licencia de conducir!");
+} else {
+console.log("Disculpa, debes esperar más años para conseguir tu licencia");
+}
+}
+
+puedeObtenerLicencia();
